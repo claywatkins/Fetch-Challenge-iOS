bdd24bef04f5ff1db4452c40dc318864527b02d1
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-09-05 18:24:35 -0700

    created dessert model

---
+++ FetchiOSChallenge/FetchiOSChallenge/Models/Dessert.swift
@@ -0,0 +1,14 @@
+//
+//  Dessert.swift
+//  FetchiOSChallenge
+//
+//  Created by Clayton Watkins on 9/5/24.
+//
+
+import Foundation
+
+struct Dessert: Decodable {
+    var strMeal: String
+    var strMealThumb: String
+    var idMeal: String
+}
