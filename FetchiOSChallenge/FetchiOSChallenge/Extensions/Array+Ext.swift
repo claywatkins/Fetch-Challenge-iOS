bdbd36ff4a7898c3356dcbfd235445c23ff20897
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-09-05 20:46:32 -0700

    added an extension to remove duplicates from an array

--- FetchiOSChallenge/FetchiOSChallenge/Extensions/Array+Ext.swift
+++ FetchiOSChallenge/FetchiOSChallenge/Extensions/Array+Ext.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension Array where Element:Equatable {
+    func removeDuplicates() -> [Element] {
+        var result = [Element]()
+
+        for value in self {
+            if result.contains(value) == false {
+                result.append(value)
+            }
+        }
+
+        return result
+    }
+}
