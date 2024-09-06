1927f85bded347bbb6d71635a6a23fa2029bf035
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-09-05 20:48:40 -0700

    created and laid out a dessert detailview. uses existing fetched details from previous view and fills in the rest when available

--- FetchiOSChallenge/FetchiOSChallenge/Views/DessertDetail.swift
+++ FetchiOSChallenge/FetchiOSChallenge/Views/DessertDetail.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct DessertDetail: View {
+    @Environment(\.desertService) var dessertService
+    var dessert: Dessert
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack {
+                    AsyncImage(url: URL(string: dessert.strMealThumb)) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                        
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    if let dessert = dessertService.dessert {
+                        VStack(alignment: .leading) {
+                            Text("Ingredients")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            ForEach(dessertService.ingredients.sorted(by: < ), id: \.key) { ingredient, measurement in
+                                HStack {
+                                    Text(ingredient)
+                                    Text(measurement)
+                                }
+                            }
+                            
+                            Text("Instructions")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            Text(dessert.strInstructions ?? "Instructions not available")
+                            
+                        }
+                    }
+                    
+                }
+                .padding(.horizontal, 12)
+                .padding(.bottom, 12)
+            }
+            .navigationTitle(dessert.strMeal)
+        }
+        .task {
+            await dessertService.fetchDetailByID(with: dessert.idMeal)
+        }
     }
+
 }
 
 #Preview {
-    DessertDetail()
+    DessertDetail(dessert: Dessert(strMeal: "Apam balik",
+                                   strMealThumb: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg",
+                                   idMeal: "53049"))
 }
