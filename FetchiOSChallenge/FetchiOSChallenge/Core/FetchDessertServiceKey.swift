fca8e30339bacd0aa69b15082cb8e64b47165505
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-09-05 18:25:01 -0700

    created an environment key for the fetch service

---
+++ FetchiOSChallenge/FetchiOSChallenge/Core/FetchDessertServiceKey.swift
@@ -0,0 +1,12 @@
+//
+//  FetchDessertServiceKey.swift
+//  FetchiOSChallenge
+//
+//  Created by Clayton Watkins on 9/5/24.
+//
+
+import SwiftUI
+
+struct FetchDessertServiceKey: EnvironmentKey {
+    static var defaultValue = FetchDessertService()
+}
